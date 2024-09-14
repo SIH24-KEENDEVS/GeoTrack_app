3087a7a2af6e158ce8ebd1b226ab18f1a5628676
RaidWLf <ambuj.kumar.7903@gmail.com>
2024-09-14 15:13:37 +0530

    Deployed & Connected Backend server

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import AttendanceReport from "./AttendanceReport";
 import UserProfile from "./Profile";
-import axios from 'axios' 
-import { createStackNavigator } from '@react-navigation/stack';
+import axios from "axios";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
@@ -19,21 +19,33 @@ const Screen = createBottomTabNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator >
-      <Stack.Screen  name="re" component={MyScreen}  options={{ 
-          headerShown: false, 
-        }}/>
-       <Stack.Screen name="AttendanceReport" component={AttendanceReport}  options={{ 
-          headerShown: false, 
-        }} />
-      <Stack.Screen name="Myroute" component={Myroute}  options={{ 
-          headerShown: false, 
-        }} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="re"
+        component={MyScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="AttendanceReport"
+        component={AttendanceReport}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Myroute"
+        component={Myroute}
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack.Navigator>
   );
 }
 export default function App() {
-  const local_coords = { latitude: 0 , longitude:  0 }; // isko manually ek fixed position ka dalana
+  const local_coords = { latitude: 0, longitude: 0 }; // isko manually ek fixed position ka dalana
   const [hasPermission, setHasPermission] = useState(false);
   const [location, setLocation] = useState({
     coords: { latitude: 0, longitude: 0 },
@@ -66,8 +78,7 @@ export default function App() {
     locationCheck();
     getLocation();
     Calculator();
-
-  },[]);
+  }, []);
 
   async function getLocation() {
     const current_loc = await Location.getCurrentPositionAsync({});
@@ -88,56 +99,72 @@ export default function App() {
   }
 
   // getLocation();
-  function Calculator(){
-    final = CalculateDistance(  
+  function Calculator() {
+    final = CalculateDistance(
       local_coords.latitude,
       local_coords.longitude,
       location.coords.latitude,
       location.coords.longitude
     );
     // console.log(final);
-    final_lat=location.coords.latitude.toString()
-    final_lon=location.coords.longitude.toString()
+    final_lat = location.coords.latitude.toString();
+    final_lon = location.coords.longitude.toString();
     const now = new Date();
     const currentDate = now.toLocaleDateString(); // Format: MM/DD/YYYY or based on locale
-    const currentTime = now.toLocaleTimeString(); 
+    const currentTime = now.toLocaleTimeString();
     if (final > 200) {
       console.log("You are away than 200m range");
-      sendAttendanceData(undefined,currentDate,currentTime,undefined,final_lat,final_lon)
-
+      sendAttendanceData(
+        undefined,
+        currentDate,
+        currentTime,
+        undefined,
+        final_lat,
+        final_lon
+      );
     } else {
       console.log("You are in  200m range");
-      sendAttendanceData(undefined,currentDate,currentTime,undefined,final_lat,final_lon)
-      
-
-
+      sendAttendanceData(
+        undefined,
+        currentDate,
+        currentTime,
+        undefined,
+        final_lat,
+        final_lon
+      );
     }
-  
-    
-
   }
-  async function sendAttendanceData(empId=1234567890, date, timeIn, timeOut, latitude, longitude) {
+  async function sendAttendanceData(
+    empId = 1234567890,
+    date,
+    timeIn,
+    timeOut,
+    latitude,
+    longitude
+  ) {
     // console.log(empId=1234567890, date='2024-09-07', timeIn='09:00', timeOut="00:00", latitude, longitude)
     try {
-      const response = await axios.patch(`http://192.168.43.193:3000/postattendance/${empId}`, {
-        date: date,
-        time_in: timeIn,
-        time_out: timeOut,
-        lat: latitude, 
-        lon: longitude 
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await axios.patch(
+        `https://backend-syfu.onrender.com/postattendance/${empId}`,
+        {
+          date: date,
+          time_in: timeIn,
+          time_out: timeOut,
+          lat: latitude,
+          lon: longitude,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      });
-  
-     
-      console.log('Response from server:', response.data);
+      );
+
+      console.log("Response from server:", response.data);
     } catch (error) {
-      console.error('Error sending attendance data:', error);
+      console.error("Error sending attendance data:", error);
     }
   }
-  
 
   return (
     <>
